8e64127f26070882fd3e4a1893a984d89e2c9838
Ali <shouni007@gmail.com>
2025-09-29 19:52:51 +0200

    Ajout de la classe PageId

---
+++ src/PageId.java
@@ -0,0 +1,32 @@
+public class PageId {
+
+    private int FileIdx;
+    private int PageIdx;
+
+    /**
+     * Constructeur de PageId
+     * Crée un nouvel identifiant de page avec les indices spécifiés
+     * @param FileIdx l'identifiant du fichier (le x dans Fx)
+     * @param PageIdx l'indice de la page dans le fichier (commence à 0)
+     */
+    public PageId(int FileIdx, int PageIdx) {
+        this.FileIdx = FileIdx;
+        this.PageIdx = PageIdx;
+    }
+
+    /**
+     * Récupère l'identifiant du fichier
+     * @return l'identifiant du fichier (le x dans Fx)
+     */
+    public int getFileIdx() {
+        return FileIdx;
+    }
+
+    /**
+     * Récupère l'indice de la page dans le fichier
+     * @return l'indice de la page (0 = première page, 1 = deuxième page, etc.)
+     */
+    public int getPageIdx() {
+        return PageIdx;
+    }
+}
